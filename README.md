3f4cb73421877c519b3ccfcae30efd23012c5d6c
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2023-02-16 10:52:55 -0300

    ViaCep com useState.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Procura Endereço
+
+Nesse projeto junto do instrutor Vinicios Neves pelo Alura+, ele ensina a como fazer um projeto com o API "Via CEP".
+
+O projeto faz parte do ensino a implementação e importância do useState no React.
+
+No arquivo além de aprender, resolvi também implementar o CSS deixando ele mais receptivo e intuitivo para todos.
+
+Projeto finalizado em 16/02/2023.
---
+++ src/App.js
@@ -0,0 +1,50 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [endereco, setEndereco] = useState({})
+
+function manipularEndereco (evento) {
+
+  const cep = evento.target.value
+    
+  setEndereco({
+      cep
+    })
+
+    if(cep && cep.length === 8) {
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(resposta => resposta.json())
+        .then(dados => {
+          setEndereco(enderecoAntigo => {
+            return {
+              ...enderecoAntigo,
+              endereco: dados.logradouro,
+              bairro: dados.bairro,
+              cidade: dados.localidade,
+              estado: dados.uf
+            }
+          })
+        })
+    } 
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Procura Endereço</h2>
+        <h5>Digite o seu CEP</h5>
+        <input placeholder='Digite o CEP' onChange={manipularEndereco}/>
+        <ul>
+          <li>CEP: {endereco.cep}</li>
+          <li>Bairro: {endereco.bairro}</li>
+          <li>Cidade: {endereco.cidade}</li>
+          <li>Estado: {endereco.estado}</li>
+          <li>Endereço: {endereco.endereco}</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
